96841f9f5df5d06e889da92dfb281ed6a4741eea
jethoe <jethoe.tang@gmail.com>
2024-08-04 01:25:47 -0400

    cars maker and model

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
+from .models import CarMake, CarModel
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -77,7 +78,7 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-def get_dealerships(request):
+# def get_dealerships(request):
     
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
@@ -91,3 +92,14 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})

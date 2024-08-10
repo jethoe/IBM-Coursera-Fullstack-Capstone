ee30836babcda6e82f3354268925aad109de70fb
jethoe <jethoe.tang@gmail.com>
2024-08-10 07:06:47 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib import admin
 # from django.utils.timezone import now
-from .models import CarMake, CarModel
-
+# from .models import CarMake, CarModel
 # Create your models here.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -11,7 +10,6 @@ class CarMake(models.Model):
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
-        
 class CarModel(models.Model):
     # Many-to-One relationship
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -24,14 +22,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+        validators=[MaxValueValidator(2023),MinValueValidator(2015)])
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)

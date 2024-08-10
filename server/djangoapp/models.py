e067432f9c2151864c836b38b4b26af6343cc06f
jethoe <jethoe.tang@gmail.com>
2024-08-10 07:49:06 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,11 +8,12 @@ from django.contrib import admin
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     # Other fields as needed
-    def __str__(self):        
+    def __str__(self):
         # Return the name as the string representation
-        return self.name  
+        return self.name
+
 
 class CarModel(models.Model):
     # Many-to-One relationship
@@ -26,14 +27,14 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[MaxValueValidator(2023), MinValueValidator(2015)])
-    
+    validators = [MaxValueValidator(2023), MinValueValidator(2015)])
+
     # Other fields as needed
     def __str__(self):
         # Return the name as the string representation
-        return self.name  
-    
-    
+        return self.name
+
+
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)
